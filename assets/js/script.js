59a53b7b57abc389d84d8543fbc413b2d55802bb
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-14 15:08:12 +0200

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/* 
+Descrizione:
+Iniziamo a lavorare alla nostra replica della nota app di messaggistica. L'esercitazione sará divisa in piú giornate, oggi iniziamo a lavorare alla prima milestone che vi
+riporto di seguito:
+
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall'utente (verdi) e dall'interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+ */
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            
+        }
+    }
+    
+}).mount('#app')
+
+
+
